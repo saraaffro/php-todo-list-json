719a3bbe6c3d426853c509266157b7bddde40e06
saraaffro <saraaffro@gmail.com>
2024-01-25 14:22:12 +0100

    feat. print todos in ul

---
+++ src/components/TodoList.vue
@@ -0,0 +1,33 @@
+<script>
+import axios from 'axios';
+
+export default{
+  name: 'TodoList',
+  data(){
+    return{
+        todos: [],
+    }
+  },
+  mounted(){
+    const t = this;
+
+    axios.get('http://localhost/php-todo-list-json/todoApi.php')
+    .then(res => {
+        console.log(res.data);
+        t.todos = res.data;
+    })
+    .catch(err => console.log(err));
+  }
+}
+</script>
+
+<template>
+    <h1>To do list</h1>
+    <ul>
+        <li v-for="(todo, index) in todos" :key="index">{{ todo.type }}</li>
+    </ul>
+</template>
+
+<style>
+
+</style>
